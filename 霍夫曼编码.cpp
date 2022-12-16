fe7165a81f7f40bac47fc996d9dc3717e0f2c88e
Luis <Luis@LAPTOP-J447I8KV>
2022-12-16 14:24:00 +0800

    添加项目文件。

---
+++ 霍夫曼编码.cpp
@@ -0,0 +1,29 @@
+﻿// 霍夫曼编码.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <stack>
+
+
+using namespace std;
+
+
+
+
+typedef struct node                 //结点结构
+{
+    node* left, * right;
+    int weight;
+    char word;
+}node,*tree;
+
+
+
+
+
+int main()
+{
+    return 0;
+}
+
+
